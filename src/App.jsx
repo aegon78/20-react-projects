d4c78c401021c89f9f7b40dc6887849d2eed8c9d
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-08-09 17:21:40 +0100

    d1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,60 @@
-import { useState } from 'react'
 import './index.css'
-
+const cards = [
+  {
+  name: "Satoshi Nakamoto",
+  bio: "fuck the government",
+  },
+  {
+  name: "Donald Trump",
+  bio: "make america great again",
+  },
+  {
+  name: "Phil Dunphy",
+  bio: "when life gives you lemons, make lemonade",
+  },
+  {
+  name: "Walter White",
+  bio: "Say my name!",
+  }
+]
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
-  return(
-    <>
-    <button className='substract-btn' onClick={()=> setCounter(prev => prev - 1)}>-</button>
-    <span className='counter'>{counter}</span>
-    <button className='add-btn' onClick={()=> setCounter(prev => prev + 1)}>+</button>
-    </>
+  return (
+    <div className="container">
+      <h1>list app</h1>
+
+      <div className="cards">
+        {cards.map(card =>
+
+          <article>
+            <h2>{card.name}</h2>
+            <p>{card.bio}</p>
+          </article>
+        
+        )}
+        {/* <article>
+          <h2>Satoshi Nakamoto</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        <article>
+          <h2>Jane Doe</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        <article>
+          <h2>Walter White</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </article>
+
+        <article>
+          <h2>Donald Trump</h2>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </article>
+ */}
+      </div>
+    </div>
   )
 }
 
