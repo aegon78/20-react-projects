fb05ae143becfd0e433704445f3bb3863ea64614
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-08-10 09:16:13 +0100

    d2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
 import './index.css'
-const cards = [
-  {
-  name: "Satoshi Nakamoto",
-  bio: "fuck the government",
-  },
-  {
-  name: "Donald Trump",
-  bio: "make america great again",
-  },
-  {
-  name: "Phil Dunphy",
-  bio: "when life gives you lemons, make lemonade",
-  },
-  {
-  name: "Walter White",
-  bio: "Say my name!",
-  }
-]
+import { cards } from './data'
 
 function App() {
 
@@ -25,34 +8,16 @@ function App() {
       <h1>list app</h1>
 
       <div className="cards">
+
         {cards.map(card =>
 
           <article>
             <h2>{card.name}</h2>
             <p>{card.bio}</p>
+            <button className="danger">Remove</button>
           </article>
         
         )}
-        {/* <article>
-          <h2>Satoshi Nakamoto</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </article>
-
-        <article>
-          <h2>Jane Doe</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </article>
-
-        <article>
-          <h2>Walter White</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </article>
-
-        <article>
-          <h2>Donald Trump</h2>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </article>
- */}
       </div>
     </div>
   )
