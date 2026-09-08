cabe02027105cc03087bef00a482575fd98c0ed5
aegon78 <aegonguru@gmail.com>
2026-09-08 13:09:37 +0100

    password generator finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './index.css';
 import { nums, lower, upper, syms } from './data';
 import React, { useState } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaRegCopy } from "react-icons/fa6";
 
 function App() {
   const [pLength, setpLength] = useState(8);
@@ -8,99 +11,120 @@ function App() {
   const [uppercase, setUppercase] = useState(false);
   const [numbers, setNumbers] = useState(false);
   const [symbols, setSymbols] = useState(false);
-  const [password, setPassword] = useState('')
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!uppercase && !lowercase && !symbols && !numbers)
+      toast.error('please select');
+  };
 
-  const generatePassword = () =>{
-    let generatedPassword = ''
-    if (lowercase) generatedPassword += lower
-    if (uppercase) generatedPassword += upper
-    if (numbers) generatedPassword += nums
-    if (symbols) generatedPassword += syms
-    setPassword(createPassword(generatedPassword))
-  }
+  const generatePassword = () => {
+    let generatedPassword = '';
+    if (lowercase) generatedPassword += lower;
+    if (uppercase) generatedPassword += upper;
+    if (numbers) generatedPassword += nums;
+    if (symbols) generatedPassword += syms;
+    setPassword(createPassword(generatedPassword));
+    if(generatedPassword !== '') toast.success('password generated!');
+  };
 
-  const createPassword = (generatedPassword) =>{
-    let password = ''
-    const generatedPasswordLength = generatedPassword.length
-    for(let i = 0; i < pLength; i++){
-      const passwordIndex = Math.floor(Math.random() * generatedPasswordLength) 
-      password += generatedPassword.toString().charAt(passwordIndex)
+  const createPassword = (generatedPassword) => {
+    let password = '';
+    const generatedPasswordLength = generatedPassword.length;
+    for (let i = 0; i < pLength; i++) {
+      password += generatedPassword
+        .toString()
+        .charAt(Math.floor(Math.random() * generatedPasswordLength));
     }
-    return password
-    
+    return password;
+  };
+
+  const copyFn = ()=>{
+    if(!password) {
+      toast.error('nothing to copy')
+    }else{
+      navigator.clipboard.writeText(password)
+      toast.success('copied to clipboard')
+    }
+
   }
 
   return (
-    <div className="container">
-      <h1>Password Generator</h1>
-      <div className="password-generator">
-        <p className="generated-password">{password}</p>
-        <form onSubmit={handleSubmit}>
-          <article>
-            <label htmlFor="password-length">Password Length</label>
-            <input
-              type="number"
-              name="password-length"
-              id="password-length"
-              min="8"
-              max="20"
-              placeholder="8-20"
-              defaultValue={length}
-              onChange={(e) => setpLength(e.target.value)}
-            />
-          </article>
-          <article>
-            <label htmlFor="lowercase">Lowercase Letters</label>
-            <input
-              type="checkbox"
-              name="lowercase"
-              id="lowercase"
-              checked={lowercase}
-              onChange={(e) => setLowercase(e.target.checked)}
-            />
-          </article>
+    <>
+      <ToastContainer theme="colored" />
+      <div className="container">
+        <h1>Password Generator</h1>
+        <div className="password-generator">
+          <div className='generated-password-container'>
+            <p className="generated-password">{password}</p>
+            <FaRegCopy className='copy-btn' onClick={copyFn}/>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <article>
+              <label htmlFor="password-length">Password Length</label>
+              <input
+                type="number"
+                name="password-length"
+                id="password-length"
+                min="8"
+                max="20"
+                placeholder="8-20"
+                defaultValue={pLength}
+                onChange={(e) => setpLength(e.target.value)}
+              />
+            </article>
+            <article>
+              <label htmlFor="lowercase">Lowercase Letters</label>
+              <input
+                type="checkbox"
+                name="lowercase"
+                id="lowercase"
+                checked={lowercase}
+                onChange={(e) => setLowercase(e.target.checked)}
+              />
+            </article>
 
-          <article>
-            <label htmlFor="uppercase">Uppercase Letters</label>
-            <input
-              type="checkbox"
-              name="uppercase"
-              id="uppercase"
-              checked={uppercase}
-              onChange={(e) => setUppercase(e.target.checked)}
-            />
-          </article>
+            <article>
+              <label htmlFor="uppercase">Uppercase Letters</label>
+              <input
+                type="checkbox"
+                name="uppercase"
+                id="uppercase"
+                checked={uppercase}
+                onChange={(e) => setUppercase(e.target.checked)}
+              />
+            </article>
 
-          <article>
-            <label htmlFor="numbers">Numbers</label>
-            <input
-              type="checkbox"
-              name="numbers"
-              id="numbers"
-              checked={numbers}
-              onChange={(e) => setNumbers(e.target.checked)}
-            />
-          </article>
+            <article>
+              <label htmlFor="numbers">Numbers</label>
+              <input
+                type="checkbox"
+                name="numbers"
+                id="numbers"
+                checked={numbers}
+                onChange={(e) => setNumbers(e.target.checked)}
+              />
+            </article>
 
-          <article>
-            <label htmlFor="symbols">Symbols</label>
-            <input
-              type="checkbox"
-              name="symbols"
-              id="symbols"
-              checked={symbols}
-              onChange={(e) => setSymbols(e.target.checked)}
-            />
-          </article>
+            <article>
+              <label htmlFor="symbols">Symbols</label>
+              <input
+                type="checkbox"
+                name="symbols"
+                id="symbols"
+                checked={symbols}
+                onChange={(e) => setSymbols(e.target.checked)}
+              />
+            </article>
 
-          <button onClick={generatePassword} type="submit">Suggest strong password</button>
-        </form>
+            <button onClick={generatePassword} type="submit">
+              Suggest strong password
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
